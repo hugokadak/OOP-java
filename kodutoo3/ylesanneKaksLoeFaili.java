e580e45c55789f74870ddaa8984bbba20eeefa0e
Hugo <119073767+hugokadak@users.noreply.github.com>
2024-03-24 14:04:23 +0200

    anita
    max wynn

--- kodutoo3/ylesanneKaksLoeFaili.java
+++ kodutoo3/ylesanneKaksLoeFaili.java
@@ -2,6 +2,8 @@ package kodu3_hugo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
@@ -16,12 +18,12 @@ public class ylesanneKaksLoeFaili {
     // <>
     static String loeFailiJaPrindi(String uusFail) {                // uusFail on faili nimi stringina
         String b = "le funny";
-
+        int mituRidaOnFailis = 0;
+        String[] eesnimedAndmed = new String[5];                // string[] et kontrolle teha
+        String[] perenimedAndmed = new String[5];
+        String[] isikukoodidAndmed = new String[5];             // valideerida isikukood
+        String[] palkAndmed = new String[5];                    // valideerida palk
         try {                                                       // try plokk for exceptions
-            String tempNimi = "hugo";                               // test string[] selle kaudu koik andmed lisada
-            String TempPere = "hugo";
-            String isikuKood = "691337";
-            String palk = "nu bagadi palk";
             File inimesteFail = new File(uusFail);                  // find file
             String[] inimeneFail = new String[5];                   // temp string arr inimeste andmete jaoks
             int index = 0;                                          // away from fail
@@ -29,41 +31,87 @@ public class ylesanneKaksLoeFaili {
             // <>
             while (scanner.hasNextLine()) {                         // loe iga rida läbi pms
                 String tekst = scanner.nextLine();                  // beem bam bop
-                System.out.println(tekst);                          // kontroll mis tekib
                 inimeneFail[index] = tekst;                         // temp string[] reast 26
-                //String[] andmed = tekst.split("|");               // split ei tööta, ja see dumbass püstkriips ei tööta ka
+                index++;            // kuidas mul see lisamata jäi?? anyways nüüd string[] töötab
+                mituRidaOnFailis++;
             }
-            for (int i = 0; i < inimeneFail.length; i++) {          // uus int testides kas saab
-                String[] eraldiInimene = inimeneFail[i].split("");  // iga arr osa splittida char-ideks
-                System.out.println(Arrays.toString(eraldiInimene));       // nah ei tööta saadab null errori
-            }                                                             // nullpointerexception inimeneFail[i] is null
-            /*
+            // <>
+            //String[] eesnimedAndmed = new String[5];                // string[] et kontrolle teha
+            //String[] perenimedAndmed = new String[5];
+            //String[] isikukoodidAndmed = new String[5];             // valideerida isikukood
+            //String[] palkAndmed = new String[5];                    // valideerida palk
+            int index2 = 0;
+            for (int i = 0; i < inimeneFail.length; i++) {
+                String[] eraldiInimene = inimeneFail[i].split("\\|"); // nt // [50104080212, Kuusk, Tanel, 1200]
+                eesnimedAndmed[index2] = eraldiInimene[2]; // [Jaak, Tanel, Joosep, 1234567, Piret]
+                perenimedAndmed[index2] = eraldiInimene[1]; // [Tamm, Kuusk, Kask, Toomas, Meri]
+                isikukoodidAndmed[index2] = eraldiInimene[0]; // [37010102702, 50104080212, 81234442345, Tamm, 00000000001]
+                palkAndmed[index2] = eraldiInimene[3]; // [4400, 1200, 300   //vale, 1100      //vale, errr   //vale]
+                index2++;
+                System.out.println("-------------------- failist leitud inimesed " + index2 + " --------------------");
+                System.out.println("Eesnimi:    " + eraldiInimene[2]);
+                System.out.println("Perenimi:   " + eraldiInimene[1]);
+                System.out.println("Isikukood:  " + eraldiInimene[0]);
+                System.out.println("Palk:       " + eraldiInimene[3]);
+            }
+            System.out.println("--------------- andmete check --------------------");
+            System.out.println("eesnimedandmed = " + Arrays.toString(eesnimedAndmed));
+            System.out.println("perenimedandmed = " + Arrays.toString(perenimedAndmed));
+            System.out.println("isikukoodandmed = " + Arrays.toString(isikukoodidAndmed));
+            System.out.println("palkandmed = " + Arrays.toString(palkAndmed));
+            System.out.println("--------------- andmete check --------------------");
+            System.out.println("--------------- for cycle --------------------");
 
-            int indexInFail = 0;
-            Scanner scanner = new Scanner(inimesteFail);            // read file w scanner
-            while (scanner.hasNextLine()) {                         // while true essentially
-                String tekst = scanner.nextLine();                  // järka rida
-                System.out.println(tekst);                          // kirjuta kontrollimiseks
 
-
-                String[] andmed = tekst.split("|");           // not working
-                System.out.println(Arrays.toString(andmed));        // kontroll
-                isikuKood = "Isikukood:         " + andmed[0];
-                TempPere = "Perekonnanimi:      " + andmed[1];
-                tempNimi = "Nimi:               " + andmed[2];
-                palk = "Palk:                   " + andmed[3];
-                System.out.println(TempPere);
-                System.out.println(tempNimi);
-                System.out.println(isikuKood);
-                System.out.println(palk);
-                System.out.println("");
-                inimeneFail[indexInFail] = TempPere;    // witnessi geniaalsust lisades terve teksti stringiks
-                */
-            //}
             scanner.close();
         } catch (FileNotFoundException e) {
-            System.out.println("ei leid faili");
+            System.out.println("ei leid faili");        // error handling faili puudus
+        } catch (Exception e) {
+            System.out.println("mingi muu viga");       // error handling misc
+        } // <>
+
+        // eesnimedAndmed perenimedAndmed isikukoodidAndmed palkAndmed
+        int[] vigasedAndmed = new int[5];
+        /* -------------------------------------------------------------------------
+        23.03.2024 seis:
+        praegu töötab ilusti, runni faili enne töötamist et tuletada meelde mida programm teeb
+        siit alumine try catch plokk kirjutab faili ilusti andmeid
+        fail asub /oop-java/oop-java idk miks seal mitte kodu3_hugo kaustas
+        teha vaja:
+        if tingimused kas perenimi on valid
+        if tingimused kas eesnimi on valid
+        if tingimused kas isikukood on valid
+        if tingimused kas palk on valid
+        kirjutada teise faili vigaste andmetega read
+        teoorias kasutades samat int array kus leiab kas anne on valid indexi kaudu lisada
+        uued andmed teise faili
+        ------------------------------------------------------------------------- */
+
+        try {
+            FileWriter failiKirjutaja = new FileWriter("valjund1.txt");
+            for (int i = 0; i < mituRidaOnFailis; i++) {
+                failiKirjutaja.write("Perenimi:     " + perenimedAndmed[i] + "\n");
+                failiKirjutaja.write("Eesnimi:      " + eesnimedAndmed[i] + "\n");
+                failiKirjutaja.write("Isikukood:    " + isikukoodidAndmed[i] + "\n");
+                failiKirjutaja.write("Palk:         " + palkAndmed[i] + "\n");
+                failiKirjutaja.write("\n");
+                failiKirjutaja.write("\n");
+            }
+            failiKirjutaja.close();
+        } catch (Exception e) {
+            System.out.println("Error: miski läks valesti aga programm ei tea mis (heh)");
         }
+
+
+        b = b + " num 69";
         return b;
     }
+    /*static void convert(String sisendfail, String valjund1, String valjund2) {
+        try {
+            File valjund1fail = new File("valjund1.txt");
+
+        } catch (Exception e) {
+            System.out.println("Error: miski läks valesti aga programm ei tea mis (heh)");
+        }
+    }*/
 }
